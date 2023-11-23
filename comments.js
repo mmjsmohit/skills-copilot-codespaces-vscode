07a1dfbe20276ca357ee40f41b068df8e36a435a
Mohit Tiwari <65406090+mmjsmohit@users.noreply.github.com>
2023-11-23 06:40:12 +0000

    Copliot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,24 @@
-//create web server
-const express = require('express');
+// Create web server
+// Run: node comments.js
+// Test: http://localhost:3000/comments
+
+// Load modules
+var http = require('http');
+var fs = require('fs');
+
+// Create web server
+http.createServer(function (req, res) {
+    // Open file
+    fs.readFile('comments.json', 'utf-8', function (err, data) {
+        // Parse JSON
+        var comments = JSON.parse(data);
+        // Set response header
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': '*'
+        });
+        // Set response content    
+        res.end(JSON.stringify(comments));
+    });
+}).listen(3000);
+console.log('Server running at http://localhost:3000/comments');
